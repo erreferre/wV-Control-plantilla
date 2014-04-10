441a2d4fe5d09187095cbea214ae7129e5053802
lia carballal <telerik@aerowi.es>
2014-04-10 12:19:59 -0500

    Changes made by lia carballal (telerik@aerowi.es).

--- wiVivo Control David y Taninos/scripts/login.js
+++ wiVivo Control David y Taninos/scripts/login.js
@@ -1,22 +1,17 @@
-//variables Globales
-//var servidor_wivivo_login = 'http://srv001.liveshowsync.local';
-var servidor_wivivo_login = 'http://192.168.10.155';
-var webservice_wivivo_login = servidor_wivivo_login + '/liveshowsync/'; 
-var servidor_login = webservice_wivivo_login + 'login.php';
-
 function init() {
 document.addEventListener("deviceready", deviceReady, true);
 delete init;
-};
+}
+
 
 function checkPreAuth() {
     var form = $("#loginForm");
-    if(window.localStorage["username"] !== undefined && window.localStorage["password"] !== undefined) {
+    if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
         $("#username", form).val(window.localStorage["username"]);
         $("#password", form).val(window.localStorage["password"]);
         handleLogin();
     }
-};
+}
 
 function handleLogin() {
     var form = $("#loginForm");    
@@ -25,26 +20,28 @@ function handleLogin() {
     var u = $("#username", form).val();
     var p = $("#password", form).val();
     console.log("click");
-    if(u !== '' && p!== '') {
-        $.post("servidor_login", {username:u,password:p}, function(res) {
-            if(res === true) {
+    if(u != '' && p!= '') {
+        $.post("http://www.coldfusionjedi.com/demos/2011/nov/10/service.cfc?method=login&returnformat=json", {username:u,password:p}, function(res) {
+            if(res == true) {
                 //store
                 window.localStorage["username"] = u;
                 window.localStorage["password"] = p;             
-                $.mobile.changePage("control.html");
+                $.mobile.changePage("some.html");
             } else {
-                navigator.notification.alert("Usuario NON correcto", function() {});
+                navigator.notification.alert("Your login failed", function() {});
             }
          $("#submitButton").removeAttr("disabled");
         },"json");
     } else {
         //Thanks Igor!
-        navigator.notification.alert("Debes introducir usuario e contrasinal", function() {});
+        navigator.notification.alert("You must enter a username and password", function() {});
         $("#submitButton").removeAttr("disabled");
     }
     return false;
-};
+}
 
 function deviceReady() {
-	$("#loginForm").on("submit",handleLogin);
+    
+$("#loginForm").on("submit",handleLogin);
+
 }
